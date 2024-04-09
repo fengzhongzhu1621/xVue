6114295ac0726cb4cc8386fa2df23f176a2453b4
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-04-09 23:14:50 +0800

    feat: add vite-plugin-checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,18 @@ import vue from "@vitejs/plugin-vue";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 import eslint from "vite-plugin-eslint2";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasm(), topLevelAwait(), eslint({ fix: true })],
+  plugins: [
+    vue(),
+    wasm(),
+    topLevelAwait(),
+    eslint({ fix: true }),
+    checker({
+      typescript: true,
+      vueTsc: true,
+    }),
+  ],
 });
