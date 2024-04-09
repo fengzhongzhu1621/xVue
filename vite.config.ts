2dcf16cd631c5be3e861e872ce335998561588df
fengzhongzhu1621 <jinyinqiao@gmail.com>
2024-04-09 23:13:31 +0800

    feat: add vite-plugin-eslint2 plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import eslint from "vite-plugin-eslint2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasm(), topLevelAwait()],
+  plugins: [vue(), wasm(), topLevelAwait(), eslint({ fix: true })],
 });
